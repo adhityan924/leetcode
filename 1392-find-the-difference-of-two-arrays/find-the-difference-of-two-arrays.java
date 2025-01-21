ad4239744c4b5e77f54cd7b54b42f7ff72b75b0e
Adhityan <adhityaa@andrew.cmu.edu>
2025-01-20 22:07:00 -0800

    Time: 9 ms (96.41%) | Memory: 45.8 MB (11.25%) - LeetSync

--- 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.java
+++ 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.java
@@ -9,20 +9,13 @@ class Solution {
         for(int n:nums2){
             n2.add(n);
         }
-        List<Integer> r1 = new ArrayList();
-        List<Integer> r2 = new ArrayList();
-
-        for(int n:nums1){
-            if(!r1.contains(n)&&!n2.contains(n)){
-                r1.add(n);
+        for (int n:nums2){
+            if(n1.contains(n)){
+                n1.remove(n);
+                n2.remove(n);
             }
         }
-        for(int n:nums2){
-            if(!r2.contains(n)&&!n1.contains(n)){
-                r2.add(n);
-            }
-        }
-        return Arrays.asList(r1,r2);
+        return Arrays.asList(new ArrayList<>(n1),new ArrayList<>(n2));
 
     }
 }
