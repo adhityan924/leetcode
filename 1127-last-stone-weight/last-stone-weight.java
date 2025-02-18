19d67a5e9096ec1e02f84e4f6f6bbba5822584de
Adhityan <adhityaa@andrew.cmu.edu>
2025-02-17 23:20:37 -0800

    Time: 1 ms (97.98%) | Memory: 41.7 MB (7.43%) - LeetSync

---
+++ 1127-last-stone-weight/last-stone-weight.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+       List<Integer> stoneList = new ArrayList<>();
+        for (int stone : stones) {
+            stoneList.add(stone);
+        }
+        Collections.sort(stoneList);
+
+        while (stoneList.size() > 1) {
+            int stone1 = stoneList.remove(stoneList.size() - 1);
+            int stone2 = stoneList.remove(stoneList.size() - 1);
+
+            if (stone1 != stone2) {
+                int newStone = stone1 - stone2;
+                int index = Collections.binarySearch(stoneList, newStone);
+                if (index < 0) {
+                    stoneList.add(-index - 1, newStone);
+                } else {
+                    stoneList.add(index, newStone);
+                }
+            }
+        }
+
+        return !stoneList.isEmpty() ? stoneList.remove(0) : 0; 
+    }
+}
