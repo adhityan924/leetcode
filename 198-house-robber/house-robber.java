09f249283f5776cb43716242748397de3a9b0a73
Adhityan <adhityaa@andrew.cmu.edu>
2025-10-02 14:39:04 -0700

    Time: 0 ms (100.00%) | Memory: 41.4 MB (25.41%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -4,13 +4,6 @@ class Solution {
         int[] stolen = new int[n+1];
         stolen[n]=0;
         stolen[n-1]=nums[n-1];
-        // for(int i=2;i<=n;i++){
-        //     stolen[i] = Math.max(stolen[i-1], (stolen[i]+stolen[i-2]));
-        // }
-        // for(int i=2;i<nums.length;i++){
-        //     nums[i] = Math.max(nums[i-1], (nums[i]+nums[i-2]));
-        // }
-
         for(int i=n-2;i>=0;i--){
             stolen[i]= Math.max(stolen[i+1], nums[i]+stolen[i+2]);
         }
